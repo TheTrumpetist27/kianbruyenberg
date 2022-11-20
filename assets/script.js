a6c6457b666bed779bdf7dcc0e1d7a5a6d4fd6c9
TheTrumpetist27 <kbruyenberg@gmail.com>
2022-11-21 00:11:16 +0100

    home header en navbar gemaakt.

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// Typing animation
+const typed = new Typed(".typing",{
+    strings:["", "Student", "Software Engineer", "Software Developer", "Web Developer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true,
+    smartBackspace:true
+})
--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,37 @@
 <body>
-    <section>
+    <header>
+        <?php include_once("includes/nav.html"); ?>
+    </header>
+
+    <section class="home-section" id="home">
+        <div class="container">
+            <div class="row">
+                <div class="col info">
+                    <div class="introduction">
+                        <h1 class="text">Hi. I'm Kian.</h1>
+                        <h1 class="text">A <span class="typing text-accent">Student</span></h1><br />
+                        <p class="home-description">I love building software for any website or application. <strong>Come on in.</strong></p>
+                    </div>
+                </div>
+                <div class="col explore">
+                    <a href="#about" class="explore-button">Explore</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="about-section" id="about">
         
     </section>
+    <section class="experience-section" id="experience">
+        
+    </section>
+    <section class="skills-section" id="skills">
+        
+    </section>
+    <?php include_once("includes/footer.html"); ?>
+
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.12/typed.min.js" referrerpolicy="no-referrer"></script>
+    <script src="assets/script.js"></script>
 </body>
