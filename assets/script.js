2142e06a321c0f41580c969563cf39797d2d153b
TheTrumpetist27 <kbruyenberg@gmail.com>
2022-11-22 09:24:19 +0100

    ja

--- assets/script.js
+++ assets/script.js
@@ -10,8 +10,20 @@ const typed = new Typed(".typing",{
 // change navbar color
 window.addEventListener('scroll', () => {
     let scroll = this.scrollY;
-    // console.log(scroll);
+    console.log(scroll);
     // The about section: 888, 1824
+    // 655, 1379
+
+    if (window.matchMedia("(min-width: 1366px)").matches) {
+        if (scroll >= 655 && scroll <= 1379) {
+            const nav = document.querySelector(":root");
+            nav.style.setProperty("--dynamic-text-color", "black");
+        } else {
+            const nav = document.querySelector(":root");
+            nav.style.setProperty("--dynamic-text-color", "white");
+        }
+    }
+
     if (scroll >= 888 && scroll <= 1824) {
         const nav = document.querySelector(":root");
         nav.style.setProperty("--dynamic-text-color", "black");
